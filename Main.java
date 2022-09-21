cbacda923bc9abc680aa3ccd3d0f84360882e79e
LivanMDC <113356765+LivanMiranda@users.noreply.github.com>
2022-09-21 12:26:04 +0000

    second assignment

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    String x;
     System.out.println("Enter a word:");
     
-    //Your code will return the length of the entered String
+   Scanner obj = new Scanner(System.in);
+    x = obj.nextLine();
+    
+    System.out.println(x.length());
+    
   }
 }
